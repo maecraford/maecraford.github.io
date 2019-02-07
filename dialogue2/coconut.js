1e6783530fae83b92b0b117c85e39ddc08d0c29e
Mae Craford <crafh792@newschool.edu>
2019-02-07 17:52:14 -0500

    a

---
+++ dialogue2/coconut.js
@@ -0,0 +1,11 @@
+var box = document.querySelector("#box")
+
+window.addEventListener("mousemove",onMouseMove)
+
+function onMouseMove(evt){
+  console.log(evt.x + " " + evt.y)
+  box.style.left = evt.x + "px"
+  box.style.top = evt.y + "px"
+
+}
+var background = document.querySelector("background")
