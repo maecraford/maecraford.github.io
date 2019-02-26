145b1f1707034b2c00cfdfe67a814ac75dfad77b
Mae Craford <crafh792@newschool.edu>
2019-02-26 17:47:42 -0500

    a

--- icebox/interact.js
+++ icebox/interact.js
@@ -5,7 +5,7 @@ function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id)) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id ).onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV: 
     elmnt.onmousedown = dragMouseDown;
