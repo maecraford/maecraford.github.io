8c070f26b75a546a911a5a1c5ce6a54be2dfcd9e
Mae Craford <crafh792@newschool.edu>
2019-02-27 02:15:39 -0500

    a

---
+++ rope/rope.js
@@ -0,0 +1,20 @@
+
+
+
+function changeImage()
+{
+element=document.getElementById('rope')
+if (element.src.match("yellow"))
+  {
+  element.src="red.png";
+  }
+else
+  {
+  element.src="yellow.png";
+  }
+}
+
+
+
+
+
