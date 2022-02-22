f9cff0c22c4f7eba28d49eed02d0e7bd2ef577bc
Mae Craford <crafh792@newschool.edu>
2022-02-22 10:47:05 -0800

    portfolio update

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -16,3 +16,11 @@
 
 
 
+function myFunction() {
+  var x = document.getElementById("about");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
